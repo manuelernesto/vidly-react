a53eac03aea58f9dafa25a9d25ee807ade3647ad
manuelernesto <manuelernesto@outlook.com>
2021-02-20 01:46:05 +0100

    add filtering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
 import {deleteMovie, getMovies} from "../services/fakeMovieService";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import {paginate} from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import {getGenres} from "../services/fakeGenreService";
+import MoviesTable from "./moviesTable";
 
 class Movies extends Component {
     state = {
@@ -27,7 +27,7 @@ class Movies extends Component {
 
     handleDelete = movie => {
         this.setState(
-            deleteMovie(movie)
+            deleteMovie(movie._id)
         );
     };
 
@@ -70,37 +70,11 @@ class Movies extends Component {
 
                 <div className="col">
                     <p>Showing {filtered.length} in the database</p>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th/>
-                            <th/>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
-                                </td>
-                                <td>
-                                    <button className="btn btn-danger btn-sm"
-                                            onClick={() => this.handleDelete(movie._id)}>
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+
+                    <MoviesTable
+                        movies={movies}
+                        onLike={this.handleLike}
+                        onDelete={this.handleDelete}/>
 
                     <Pagination
                         itemsCount={filtered.length}
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Like from "./common/like";
+
+const MoviesTable = (props) => {
+    const {movies, onDelete, onLike} = props;
+
+    return (
+        <table className="table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th/>
+                <th/>
+            </tr>
+            </thead>
+            <tbody>
+            {movies.map(movie => (
+                <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                        <Like liked={movie.liked} onClick={() =>onLike(movie)}/>
+                    </td>
+                    <td>
+                        <button className="btn btn-danger btn-sm"
+                                onClick={() => onDelete(movie)}>
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default MoviesTable;
